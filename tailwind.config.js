3f8f891fc7c543611142c29965fbbefad0a015be
Mati Rapoport <mati.rapoport@gmail.com>
2024-08-09 01:44:59 -0300

    fix tailwind vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: [
     "./app/**/page.{js,ts,jsx,tsx,mdx}",
+    "./app/**/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+
   ],
   theme: {
     extend: {},
